78d901fe9b9565412f7308a8f030d59937acd196
simon.chen <lovelife0314@163.com>
2019-07-09 22:47:52 +0800

    update for vcpcs 182;apply new validation method

--- gui.py
+++ gui.py
@@ -8,20 +8,19 @@
 ###########################################################################
 
 import wx
-import wx.richtext
+#from wx.tools.img2py import img2py
+import visa
 
-
-###########################################################################
-## Class MyFrame1
-###########################################################################
+# use img2py transfer image to py
+#ret = img2py('C:\\Users\\user\\Desktop\\hce_rrap\\visa.png', 'C:\\Users\\user\\Desktop\\hce_rrap\\visa.py', append=True)
 
 class MyFrame1(wx.Frame):
 
 	def __init__(self, parent):
-		wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"HCE RRAP TOOL v2.0", pos=wx.DefaultPosition,
-						  size=wx.Size(500, 600), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+		wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"RRAP_HCE_181a_R0", pos=wx.DefaultPosition,
+						  size=wx.Size(-1, -1), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
 
-		self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+		self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
 
 		fgSizer1 = wx.FlexGridSizer(2, 2, 0, 0)
 		fgSizer1.SetFlexibleDirection(wx.BOTH)
@@ -31,51 +30,65 @@ class MyFrame1(wx.Frame):
 		gbSizer1.SetFlexibleDirection(wx.BOTH)
 		gbSizer1.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
-		self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"Test Tool Report", wx.DefaultPosition, wx.DefaultSize, 0)
+		self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"Test Tool Vendor", wx.DefaultPosition, wx.DefaultSize, 0)
 		self.m_staticText1.Wrap(-1)
-		gbSizer1.Add(self.m_staticText1, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL, 8)
+		gbSizer1.Add(self.m_staticText1, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.ALL, 8)
+
+		self.m_staticText11 = wx.StaticText(self, wx.ID_ANY, u"Test Tool Report path", wx.DefaultPosition,
+											wx.DefaultSize, 0)
+		self.m_staticText11.Wrap(-1)
+		gbSizer1.Add(self.m_staticText11, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL, 8)
 
 		inputChoiceChoices = [u"ICCSolutions", u"Galitt", u"UL"]
 		self.inputChoice = wx.Choice(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, inputChoiceChoices, 0)
 		self.inputChoice.SetSelection(1)
-		gbSizer1.Add(self.inputChoice, wx.GBPosition(0, 1), wx.GBSpan(1, 1), wx.ALL, 6)
+		gbSizer1.Add(self.inputChoice, wx.GBPosition(1, 2), wx.GBSpan(1, 1), wx.ALL, 6)
 
 		self.xmlButton = wx.Button(self, wx.ID_ANY, u"Generate XML Analyze Result", wx.Point(-1, -1), wx.Size(220, -1),
 								   0)
 		self.xmlButton.SetDefault()
-		gbSizer1.Add(self.xmlButton, wx.GBPosition(2, 2), wx.GBSpan(1, 1), wx.ALL, 5)
-
-		self.executeLog = wx.richtext.RichTextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
-												   wx.Size(250, 450),
-												   0 | wx.VSCROLL | wx.HSCROLL | wx.NO_BORDER | wx.WANTS_CHARS)
-		gbSizer1.Add(self.executeLog, wx.GBPosition(1, 0), wx.GBSpan(1, 3), wx.EXPAND | wx.ALL, 5)
+		gbSizer1.Add(self.xmlButton, wx.GBPosition(4, 2), wx.GBSpan(1, 1), wx.ALL, 5)
 
 		self.pdfButton = wx.Button(self, wx.ID_ANY, u"Generate PDF Analyze Result", wx.Point(-1, -1), wx.Size(220, -1),
 								   0)
 		self.pdfButton.SetDefault()
-		gbSizer1.Add(self.pdfButton, wx.GBPosition(3, 2), wx.GBSpan(1, 1), wx.ALL, 5)
+		gbSizer1.Add(self.pdfButton, wx.GBPosition(5, 2), wx.GBSpan(1, 1), wx.ALL, 5)
 
-		gbSizer1.Add((0, 310), wx.GBPosition(1, 3), wx.GBSpan(1, 1), wx.EXPAND, 5)
+		self.executeLog = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(500, 400), style=wx.TE_MULTILINE )
+		gbSizer1.Add(self.executeLog, wx.GBPosition(3, 0), wx.GBSpan(1, 5), wx.ALL, 5)
+
+		gbSizer1.Add((0, 310), wx.GBPosition(3, 5), wx.GBSpan(1, 1), wx.EXPAND, 5)
 
 		self.inputPath = wx.FilePickerCtrl(self, wx.ID_ANY, wx.EmptyString, u"Select a file", u"*.*",
 										   wx.DefaultPosition, wx.DefaultSize, wx.FLP_DEFAULT_STYLE)
-		gbSizer1.Add(self.inputPath, wx.GBPosition(0, 2), wx.GBSpan(1, 2), wx.ALL, 1)
+		gbSizer1.Add(self.inputPath, wx.GBPosition(2, 2), wx.GBSpan(1, 2), 0, 1)
+
+		self.m_bitmap3 = wx.StaticBitmap(self, wx.ID_ANY, visa.visa.getBitmap(),
+										 wx.DefaultPosition, wx.DefaultSize, 0)
+		gbSizer1.Add(self.m_bitmap3, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+		# self.m_staticText6 = wx.StaticText(self, wx.ID_ANY, u"Developed by", wx.DefaultPosition, wx.DefaultSize,
+		# 								   0)
+		# self.m_staticText6.Wrap(-1)
+		# self.m_staticText6.SetFont(wx.Font(wx.NORMAL_FONT.GetPointSize(), 70, 90, 92, False, wx.EmptyString))
+		# self.m_staticText6.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_GRAYTEXT))
+		# gbSizer1.Add(self.m_staticText6, wx.GBPosition(4, 0), wx.GBSpan(1, 1), wx.ALIGN_BOTTOM|wx.ALL, 5)
+		#
+		# #image = wx.Image(u"applus.png", wx.BITMAP_TYPE_ANY)
+		# image = applus.applus.getBitmap().ConvertToImage()
+		# image.Rescale(image.GetWidth()/2.5, image.GetHeight()/2.5)
+		# self.m_bitmap4 = wx.StaticBitmap(self, wx.ID_ANY, image.ConvertToBitmap(),
+		# 								 wx.DefaultPosition, wx.DefaultSize, 0)
+		#
+		# gbSizer1.Add((0, 5), wx.GBPosition(6, 6), wx.GBSpan(1, 1), wx.EXPAND, 5)
+		#
+		# gbSizer1.Add(self.m_bitmap4, wx.GBPosition(5, 0), wx.GBSpan(1, 1), wx.ALL, 5)
 
 		fgSizer1.Add(gbSizer1, 1, wx.EXPAND, 5)
 
 		self.SetSizer(fgSizer1)
 		self.Layout()
-		self.m_menubar1 = wx.MenuBar(0)
-		self.m_menu1 = wx.Menu()
-		self.menuManual = wx.MenuItem(self.m_menu1, wx.ID_ANY, u"Manual", wx.EmptyString, wx.ITEM_NORMAL)
-		self.m_menu1.Append(self.menuManual)
-
-		self.menuAbout = wx.MenuItem(self.m_menu1, wx.ID_ANY, u"About", wx.EmptyString, wx.ITEM_NORMAL)
-		self.m_menu1.Append(self.menuAbout)
-
-		self.m_menubar1.Append(self.m_menu1, u"Help")
-
-		self.SetMenuBar(self.m_menubar1)
+		fgSizer1.Fit(self)
 
 		self.Centre(wx.BOTH)
 
@@ -86,7 +99,7 @@ class MyFrame1(wx.Frame):
 	def __del__(self):
 		pass
 
-	# Virtual event handlers, overide them in your derived class
+	# Virtual event handlers, override them in your derived class
 	def genXmlReport(self, event):
 		self.genCompareReport('xml')
 
@@ -95,6 +108,4 @@ class MyFrame1(wx.Frame):
 
 	def genCompareReport(self, format):
 		pass
-
-
 
